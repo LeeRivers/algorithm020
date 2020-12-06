b4b18cbd50eed56caa45d344fd3c4983de15e1f5
LeeRivers <lxc_sdu@163.com>
2020-12-06 22:22:47 +0800

    [week-3] 添加作业与学习笔记

---
+++ Week3/77-组合.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        if n < k or n < 1:
+            return []
+        if k == 0:
+            return [[]]
+        if n == k:
+            return [[i for i in range(1, n+1)]]
+        ans1 = self.combine(n-1, k-1)
+        ans2 = self.combine(n-1, k)
+        print(n, k, ans1, ans2)
+        if ans1:
+            for i in ans1:
+                i.append(n)
+        return ans1+ans2
